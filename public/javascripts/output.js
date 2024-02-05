cdb568d88460a6d5011a7190710df1346f54bc85
parth3083 <parthbrajput30@gmail.com>
2024-02-05 18:00:41 +0530

    Updated the output template animation

--- public/javascripts/output.js
+++ public/javascripts/output.js
@@ -3,4 +3,11 @@ tl.to("#one", {
     left: "0%",
     ease: Power2,
     duration: 2
-});
+},"a");
+tl.to('body', {
+    opacity: 1,
+    // backgroundColor:"#fff",
+    duration: 2.5,
+    z:'10%',
+    ease: Power2
+},"a")
